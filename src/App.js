4a1f8405e8f9191a8ac3558fb6c3729c4d9bba72
Anakagung013 <anakagungputugedejayanugraha@gmail.com>
2024-09-28 17:20:33 +0800

    Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // src/App.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // Pastikan mengimpor useState dan useEffect
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import CurrencyConverter from './components/CurrencyConverter';
 import DataStorageConverter from './components/DataStorageConverter';
 import Navbar from './components/Navbar';
-import CustomCursor from './components/CustomCursor';
+import CustomCursor from './components/CustomCursor'; // Sesuaikan dengan path yang diperlukan
 import Footer from './components/Footer';
 import About from './components/About';
-import LoadingScreen from './components/LoadingScreen';
+import LoadingScreen from '../src/components/LoadingScreen';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -17,19 +18,19 @@ const App = () => {
     // Simulasikan loading screen selama 2 detik
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 2000); // Durasi loading dalam milidetik
 
     // Bersihkan timer saat komponen unmount
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <BrowserRouter basename="/toolsconvert"> {/* Menambahkan basename di sini */}
+    <BrowserRouter>
       {loading ? (
-        <LoadingScreen />
+        <LoadingScreen /> // Hanya menampilkan LoadingScreen saat loading true
       ) : (
         <>
-          <Navbar />
+          <Navbar /> {/* Navbar ditempatkan di sini */}
           <CustomCursor />
           <Routes>
             <Route path="/" element={<LandingPage />} />
